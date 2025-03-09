c1467fb4da2105cdfc3d361ed7c7ae58d2e4e5fb
Schreiry <108213850+Schreiry@users.noreply.github.com>
2025-03-09 20:11:15 +0400

    Constant.h

--- Constant.h
+++ Constant.h
@@ -1,21 +1,21 @@
 #pragma once
 
-// Constants.h
+//Directive
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-// Размер одного блока (в пикселях)
+// Size of one block (in pixels)
 const int blockSize = 30;
 
-// Размеры игрового поля (в блоках)
+// Playing field dimensions (in blocks)
 const int gridWidth = 15;
 const int gridHeight = 25;
 
-// Отступ игрового поля от краёв окна (в пикселях)
+// Indentation of the playing field from the window edges (in pixels)
 const int fieldOffsetX = 50;
 const int fieldOffsetY = 50;
 
-// Размеры окна: игровое поле + дополнительная область для отчётов и превью
+// Window size: playing field + additional area for reports and previews
 const int windowWidth = fieldOffsetX + gridWidth * blockSize + 200;
 const int windowHeight = fieldOffsetY + gridHeight * blockSize + 50;
 
